70afb7cb793ab84b414b7d235210d022f955c942
Eduardo Silva <eduardofox989@protonmail.com>
2024-09-16 11:39:29 -0300

    add emacs configs

--- modules/default.nix
+++ modules/default.nix
@@ -8,6 +8,12 @@ inputs.home-manager.lib.homeManagerConfiguration {
   pkgs = import inputs.nixpkgs {
     inherit system;
     config.allowUnfree = true;
+    overlays = [
+      (import (builtins.fetchTarball {
+        url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+        sha256 = "1h59k5anc01x7k147c09kn5fmypv01x1pcmjw99jixxqs150qdig";
+      }))
+    ];
   };
   extraSpecialArgs = {inherit inputs outputs;};
   modules = [./home.nix];
--- modules/home.nix
+++ modules/home.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [
     ./editors/emacs.nix
     ./editors/neovim.nix
@@ -10,16 +10,31 @@
     username = "edu";
     homeDirectory = "/home/edu";
     stateVersion = "24.05";
+    file.".emacs.d" = {
+      source = ../config/.emacs.d;
+      recursive = true;
+    };
   };
 
   programs.home-manager.enable = true;
 
+  # services.emacs.package = pkgs.emacs-unstable;
+
   programs.emacs = {
     enable = true;
-    extraPackages = epkgs: [
-      epkgs.nix-mode
-      epkgs.magit
-    ];
+    package = pkgs.emacs-git;
+    extraPackages = epkgs:
+      with epkgs; [
+        nix-mode
+        magit
+        org-contrib
+        evil
+        evil-collection
+        company
+        eglot
+        format-all
+        flycheck
+      ];
   };
 
   services.gpg-agent = {
