a2ba0570fec52b2d8482156bc28808095d140b68
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-17 19:45:53 -0300

    hostname change and configuration cleanup, added more configuration in modules

---
+++ hosts/nagakiba/README.md
@@ -0,0 +1,4 @@
+modelo lenovo ideaped 3
+CPU ryzen 5500u
+RAM 8gbs ddr4
+HDD 256gbs NVMe SSD
--- hosts/nagakiba/default.nix
+++ hosts/nagakiba/default.nix
@@ -1,22 +1,68 @@
-{
-  inputs,
-  system,
-  ...
-}:
-#TODO passa a usar a definição pkgs, pkgs-unstable que esta no flake
-inputs.nixpkgs.lib.nixosSystem {
-  specialArgs = {
-    pkgs = import inputs.nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
+{lib, ...}:
+with lib;
+with builtins; {
+  system = "x86_64-linux";
+
+  networking.hostName = "nagakiba";
+
+  config = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    services.fstrim.enable = lib.mkDefault true;
+    hardware.amdgpu.initrd.enable = lib.mkDefault true;
+    hardware.graphics = {
+      enable = lib.mkDefault true;
+      enable32Bit = lib.mkDefault true;
+      extraPackages = with pkgs; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+      ];
     };
-    pkgs-unstable = import inputs.nixpkgs-unstable {
-      inherit system;
-      config.allowUnfree = true;
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/b71e7e70-af18-4de2-bc98-bf4954832d9e";
+        fsType = "btrfs";
+        options = ["subvol=@"];
+      };
+
+      "/boot" = {
+        device = "/dev/disk/by-uuid/9047-DC7B";
+        fsType = "vfat";
+        options = ["fmask=0022" "dmask=0022"];
+      };
     };
-    inherit inputs system;
+
+    swapDevices = [
+      {
+        device = "/dev/nvme0n1p3";
+        randomEncryption.enable = true;
+      }
+    ];
+
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+      kernelPackages = pkgs.linuxPackages_latest;
+      kernelModules = ["kvm-amd"];
+      extraModulePackages = [];
+      initrd = {
+        availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "ahci"
+          "usb_storage"
+          "sd_mod"
+        ];
+        kernelModules = ["amdgpu"];
+      };
+      extraModprobeConfig = ''
+        drm_kms_helper
+        options drm_kms_helper poll=N
+      '';
+    };
   };
-  modules = [
-    ./configuration.nix
-  ];
 }
