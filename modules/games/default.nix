e129f0d4008ae45cd1cd22e0b97a76239e112d28
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-18 00:44:13 -0300

    add config steam

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -1,12 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     wineWowPackages.wayland
-    steam
     prismlauncher
     duckstation
     heroic
     gogdl
-    gamescope
     (retroarch.override {cores = with libretro; [parallel-n64];})
   ];
 }
