851d85f51b0b4877aeb3e619a0bbd95d842ae3df
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-22 12:44:10 -0300

    add more configuration on module steam

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -1,9 +1,29 @@
-{
+{pkgs, ...}: {
   program = {
     steam = {
       enable = true;
       remotePlay.openFirewall = false;
+      protontricks.enable = true;
     };
-    gamescope.enable = true;
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          inhibit_screensaver = 0;
+          renice = 10;
+        };
+      };
+    };
+    gamescope = {
+      enable = true;
+    };
   };
+  user.extraGroups = ["gamemode"];
+  home.packages = with pkgs; [
+    mangohud
+    goverlay
+  ];
+
+  # Better for steam proton games
+  systemd.extraConfig = "DefaultLimitNOFILE=1048576";
 }
