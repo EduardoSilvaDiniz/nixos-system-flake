6d3ae23585594f8c2836686c98ff05cc0adde5c8
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-22 13:16:21 -0300

    fix module emulatores
    for some reason the home manager can't install stable packages so I
    moved them all to unstable, there was an error in the retroarch about
    override so it was moved to package.nix

--- modules/desktop/apps/emulatores.nix
+++ modules/desktop/apps/emulatores.nix
@@ -1,18 +1,8 @@
-{
-  pkgs,
-  pkgs-unstable,
-  ...
-}: {
-  home.packages =
-    [
-      (with pkgs; [
-        dolphin-emu
-        (retroarch.override {cores = with libretro; [parallel-n64];})
-      ])
-    ]
-    ++ (with pkgs-unstable; [
-      duckstation
-      pcsx2
-      rpcs3
-    ]);
+{pkgs-unstable, ...}: {
+  home.packages = with pkgs-unstable; [
+    duckstation
+    pcsx2
+    rpcs3
+    dolphin-emu
+  ];
 }
