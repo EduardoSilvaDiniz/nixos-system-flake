5a3f146255b61bd7bd5534b864d59e8be671adfb
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-22 12:45:15 -0300

    change order code

--- modules/default.nix
+++ modules/default.nix
@@ -1,11 +1,23 @@
 {
-  pkgs,
-  pkgs-unstable,
-  inputs,
-  ...
-}:
-inputs.home-manager.lib.homeManagerConfiguration {
-  inherit pkgs;
-  extraSpecialArgs = {inherit pkgs-unstable;};
-  modules = [./home.nix];
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "edu";
+    homeDirectory = "/home/edu";
+    stateVersion = "24.11";
+    sessionPath = ["/home/edu/.config/emacs/bin" "/home/edu/.yarn/bin"];
+  };
+
+  imports = [
+    ./editors
+    ./shell
+    ./desktop/apps/emulatores.nix
+    ./desktop/apps/launchers.nix
+    ./desktop/apps/minecraft.nix
+    ./desktop/apps/steam.nix
+    ./desktop/apps/wine.nix
+    ./desktop/term
+    ./packages.nix
+    ./desktop/term/wezterm.nix
+  ];
 }
