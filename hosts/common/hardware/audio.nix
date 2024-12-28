0ecf6eb7c9cb78b603e091f5ef37031b5cec0112
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:33:54 -0300

    remove wireplumber

--- hosts/common/hardware/audio.nix
+++ hosts/common/hardware/audio.nix
@@ -1,13 +1,11 @@
 {lib, ...}: {
+  hardware.pulseaudio.enable = lib.mkForce false;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  security.rtkit.enable = true;
   users.users.edu.extraGroups = ["audio"];
-  hardware.pulseaudio.enable = lib.mkForce false;
-  services.pipewire.wireplumber.enable = true;
 }
