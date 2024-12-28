ea1ebff0da5c73f076b65ebc165bed361c544a59
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:37:35 -0300

    add module shell

--- modules/dev/shell.nix
+++ modules/dev/shell.nix
@@ -1,3 +1,11 @@
-
-#TODO adicionar configuração
-{}
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    bashInteractive # bash
+    nodePackages_latest.bash-language-server # LSP
+    # DAP não existe, talvez seja outro nome...
+    ### Format
+    shellharden
+    shfmt
+    dotenv-linter # Diagnostics
+  ];
+}
