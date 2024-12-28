80631e2d2f154d1d32e2d9b9136f5f90081cfe42
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:38:51 -0300

    add packages on module git

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -1,7 +1,15 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     userName = "Eduardo Silva";
     userEmail = "eduardofox989@protonmail.com";
   };
+  home.packages = with pkgs; [
+    git-annex
+    gh
+    git-open
+    diff-so-fancy
+    git-crypt
+    act
+  ];
 }
