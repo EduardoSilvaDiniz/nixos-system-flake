d3d4a70cebaf6275df3c9f3d9a5864d8baa9b061
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:36:18 -0300

    add module cc

--- modules/dev/cc.nix
+++ modules/dev/cc.nix
@@ -1,2 +1,17 @@
-#TODO adicionar configuração
-{}
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # gcc
+    clang-tools
+    cmake # C/C++
+    gnumake # C/C++
+    bear # C/C++
+    cling # C/C++
+    gdb
+    clang
+    ccls # LSP
+    lldb # DAP
+    cppcheck # Diagnostics
+    checkmake
+    cmake-format
+  ];
+}
