b2d2392aed79d79cf21d94f72c5c76bfcb8b3d5f
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:36:56 -0300

    add module java

--- modules/dev/java.nix
+++ modules/dev/java.nix
@@ -1,3 +1,17 @@
-
-#TODO adicionar configuração
-{}
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    gradle # java
+    jdk
+    jdk8
+    jdt-language-server # LSP
+    # vscode-extensions.vscjava.vscode-java-test
+    # vscode-extensions.vscjava.vscode-java-debug
+    # vscode-extensions.vscjava.vscode-gradle
+    lombok
+    google-java-format # format
+    ### Diagnostics
+    checkstyle
+    pmd
+    # graalvm-ce # Extra
+  ];
+}
