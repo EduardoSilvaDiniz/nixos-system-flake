c94a07d08b4e3712748b07d162a31442e87690d1
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:50:00 -0300

    remove import

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  pkgs-unstable,
-  ...
-}: {
+{pkgs, ...}: {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-git;
@@ -13,5 +9,4 @@
         mu4e
       ];
   };
-  home.packages = import ./coding-tools.nix {inherit pkgs pkgs-unstable;};
 }
