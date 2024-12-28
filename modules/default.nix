ecd08eb7729f56f7c33c36d99af68468cba5e3c6
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:50:16 -0300

    add import all dev modules

--- modules/default.nix
+++ modules/default.nix
@@ -15,9 +15,29 @@
     ./desktop/apps/wine.nix
     ./desktop/term/wezterm.nix
     ./desktop/browsers/chrome.nix
+
     ./editors/neovim.nix
+
     ./shell/zsh.nix
     ./shell/git.nix
+
+    ./dev/cc.nix
+    ./dev/clojure.nix
+    # ./dev/docker.nix
+    ./dev/editorconfig.nix
+    # ./dev/go.nix
+    # ./dev/html.nix
+    ./dev/java.nix
+    # ./dev/javascript.nix
+    ./dev/lua.nix
+    # ./dev/markdown.nix
+    ./dev/nix.nix
+    # ./dev/php.nix
+    # ./dev/python.nix
+    # ./dev/rust.nix
+    # ./dev/shell.nix
+    # ./dev/sql.nix
+
     ./packages.nix
   ];
 }
