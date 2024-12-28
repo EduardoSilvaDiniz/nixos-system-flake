d8b6d25e0f43f0931cc28e78aa95233c9ab4f463
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:40:08 -0300

    remove unnecessary packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -12,74 +12,26 @@
   });
 in {
   home.packages = with pkgs; [
-    ## Aplications
-    (retroarch.override {cores = with libretro; [parallel-n64];}) ## só funciona aqui
+    (retroarch.override {cores = with libretro; [parallel-n64];})
     keepassxc
     thunderbird
-    vlc
-    zoom-us
+
     dbeaver-bin
+
+    zoom-us
+
     discord
+
     obsidian
     flowtime
     foliate
-    tor-browser
+
     gimp
     pdfarranger
     obs-studio
-    google-chrome
-    stremio
-    just
+
     pcloudFixes
+    
     qbittorrent
-    # gcc
-    # binutils
-    clang-tools
-    mpv
-
-    ## Compilers/interpreters
-    rustc
-    cargo
-    # Lua está na configuração do neovim
-    gdb
-    nodejs
-    go
-    # php
-    python3Full
-    glslang
-    gh-markdown-preview
-    gmp
-
-    ## Project manager
-    cmake # C/C++
-    gnumake # C/C++
-    bear # C/C++
-    leiningen # clojure
-    gradle # java
-    nodejs_22 # javascript
-    # Luarocks está na configuração do neovim
-    cargo # Rust
-
-    ## REPL
-    bashInteractive # bash
-    cling # C/C++
-    # Clojure vem do leiningen
-    # Java vem do openjdk
-    # Javascript vem do nodejs_22
-    gore # Go
-    # php83Packages.psysh # Php
-    python312Packages.ptpython # Python
-    # Rust vem do cargo
-    # Lua vem do luarocks
-
-    ## Removido do system
-    # clang
-    #TODO resolver as colições
-    # gcc # colisão com binutils
-    # binutils ## colisão com gcc
-    # jdk ## colisão com graalvm-ce
-    # jdk8
-    # php83Packages.composer # colisão com flutter
-    # php83Packages.composer
   ];
 }
