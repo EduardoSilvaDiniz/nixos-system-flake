068b82db34d533788c809f2a3cb0c05bd67fd554
Eduardo Silva <eduardofox989@protonmail.com>
2024-12-28 15:37:18 -0300

    add module php

--- modules/dev/php.nix
+++ modules/dev/php.nix
@@ -1,3 +1,15 @@
-
-#TODO adicionar configuração
-{}
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    php
+    php83Packages.psysh # Php
+    php83Packages.composer
+    phpactor # LSP
+    vscode-extensions.xdebug.php-debug # DAP
+    # Format
+    php83Packages.php-codesniffer
+    php83Packages.php-cs-fixer
+    # Diagnostics
+    php83Packages.phpstan
+    php83Packages.phpmd
+  ];
+}
