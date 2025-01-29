6e6f002dae4f71a84d05be5720d28ff08390da85
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-29 14:45:56 +0000

    add module nixos-wsl

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +13,7 @@
     self,
     nixpkgs,
     nixpkgs-unstable,
+    nixos-wsl,
     home-manager,
     ...
   } @ inputs: let
@@ -43,6 +45,7 @@
     nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        nixos-wsl.nixosModules.default
         ./hosts/wsl
       ];
     };
--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -2,14 +2,14 @@
 # https://github.com/nix-community/NixOS-WSL
 
 { config, lib, pkgs, ... }:{
-  imports = [ <nixos-wsl/modules> ];
+#  imports = [ <nixos-wsl/modules> ];
   wsl.enable = true;
   wsl.defaultUser = "nixos";
-networking.hostName = "nixos";
+  networking.hostName = "wsl";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "24.11";
-environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     neovim
 	git
-];
+  ];
 }
