9895bfb6c746fbca66d6f22c39a37822f6952e6a
Eduardo Silva <eduardofox989@protonmail.com>
2025-01-30 17:51:31 +0000

    add packages to wsl

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,15 +1,46 @@
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:{
-#  imports = [ <nixos-wsl/modules> ];
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   wsl.enable = true;
   wsl.defaultUser = "nixos";
   networking.hostName = "wsl";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   system.stateVersion = "24.11";
-  environment.systemPackages = with pkgs; [
-    neovim
-	git
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      git
+      go
+      gore # Go
+      gopls # LSP
+      delve # DAP
+      gotests # Test
+      ### Formats
+      gofumpt # Enforce a stricter format than gofmt, while being backwards compatible.
+      golines # long lines
+      gotools # adds several tools (goimports)
+      goimports-reviser
+      golangci-lint # diagnostics
+      gomodifytags # code actions
+      lua-language-server # LSP
+      stylua # format
+      nil # LSP
+      alejandra # format
+      deadnix # diagnostics
+      statix # code action
+      vimPlugins.nvim-treesitter.withAllGrammars
+      uncrustify
+      chafa
+      ueberzugpp
+      sqlite
+      tree-sitter
+      lazygit
+      hpx
+      neovim-unwrapped
+    ])
+    ++ (with pkgs.lua51Packages; [
+      lua
+      luarocks
+    ]);
 }
