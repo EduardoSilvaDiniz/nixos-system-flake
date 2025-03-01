e0e0f3cbb8b4597648b43adecc541097cc6a74ba
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-01 14:44:01 -0300

    add packages for hyprland and set dark mode to gtk 4.0

--- hosts/common/desktops/hyprland.nix
+++ hosts/common/desktops/hyprland.nix
@@ -1,5 +1,8 @@
-{pkgs, ...}: {
-  programs.hyprland.enable = true;
+{pkgs, inputs, ...}: {
+  programs.hyprland = { 
+  	enable = true;
+	xwayland.enable = true;
+  };
   services.greetd = {
     enable = true;
     settings = {
@@ -9,4 +12,34 @@
       };
     };
   };
+  environment.systemPackages = with pkgs; [
+    wofi
+    networkmanagerapplet
+    brightnessctl
+    playerctl
+    waybar
+    wlrctl
+    flameshot
+    blueman
+    mpv
+    imv
+    hyprlock
+    hypridle
+    hyprpaper
+    nautilus
+    mupdf
+    gsettings-qt
+  ];
+
+  programs.dconf.profiles.user = {
+     databases = [{
+       settings = {
+         "org/gnome/desktop/interface" = {
+           color-scheme = "prefer-dark";
+           clock-format = "12h";
+           clock-show-weekday = true;
+         };
+       };
+     }];
+   };
 }
