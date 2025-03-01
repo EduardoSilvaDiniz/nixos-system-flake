850872517e52a5be0580d7e51ffd85d9cb54e03e
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-01 14:48:01 -0300

    add typescript

--- modules/dev/javascript.nix
+++ modules/dev/javascript.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     nodejs
+    typescript
     nodePackages.typescript-language-server # LSP
     vscode-js-debug # js DAP
     nodePackages_latest.prettier # format
