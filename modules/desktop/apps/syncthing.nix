428a077f0e1472aa660ed7541d34af742454a66e
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-08 12:35:03 -0300

    move syncthing to home manager

---
+++ modules/desktop/apps/syncthing.nix
@@ -0,0 +1,11 @@
+{
+  services.syncthing = {
+    enable = true;
+		extraOptions = ''
+		<folders>
+    	<folder id="default-folder-id" label="Sync" path="/home/edu/Sync" ...
+    	</folder>
+		</folders>
+		'';
+  };
+}
