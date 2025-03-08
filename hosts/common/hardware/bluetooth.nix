78ef7e5c0f52c1096aa14fbda5b3643be04eac3d
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-08 12:22:40 -0300

    remove disable latency
    has been removed as it is a useful setting only for the xbox controller

--- hosts/common/hardware/bluetooth.nix
+++ hosts/common/hardware/bluetooth.nix
@@ -1,12 +1,5 @@
 {pkgs, ...}: {
   hardware.bluetooth.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      MinConnectionInterval = 7;
-      MaxConnectionInterval = 9;
-      ConnectionLatency = 0;
-    };
-  };
   powerManagement.resumeCommands = ''
     ${pkgs.util-linux}/bin/rfkill block bluetooth
     ${pkgs.util-linux}/bin/rfkill unblock bluetooth
