a6a05111085e7825cba847ca512d19f331d24385
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-08 14:50:23 -0300

    format default

--- home/default.nix
+++ home/default.nix
@@ -8,39 +8,76 @@
   };
 
   imports = [
-    # ./desktop/apps/emulatores.nix
-    # ./desktop/apps/launchers.nix
-    ./desktop/apps/minecraft.nix
-    # ./desktop/apps/wine.nix
-    # ./desktop/term/wezterm.nix
-     ./desktop/term/kitty.nix
-    ./desktop/browsers/edge.nix
-
-    ./editors/neovim.nix
-    ./editors/emacs.nix
-    # ./editors/jetbrains.nix
-    # ./editors/emacs.nix
-
+    ## browsers
+    ./browsers/chrome.nix
+    ./browsers/edge.nix
+    ./browsers/firefox.nix
+    ## Dev
+    ./dev/git.nix
+    #### db-admin
+    ./dev/db-admin/mongocompass.nix
+    ./dev/db-admin/pgadmin.nix
+    #### editor
+    ./dev/editor/emacs.nix
+    ./dev/editor/neovim.nix
+    ./dev/editor/jetbrains.nix
+    ./dev/editor/vscode.nix
+    #### language
+    ./dev/language/cc.nix
+    ./dev/language/clojure.nix
+    ./dev/language/docker.nix
+    ./dev/language/editorconfig.nix
+    ./dev/language/go.nix
+    ./dev/language/html.nix
+    ./dev/language/java.nix
+    ./dev/language/javascript.nix
+    ./dev/language/lua.nix
+    ./dev/language/markdown.nix
+    ./dev/language/nix.nix
+    ./dev/language/php.nix
+    ./dev/language/python.nix
+    ./dev/language/rust.nix
+    ./dev/language/shell.nix
+    ./dev/language/sql.nix
+    ## Games
+    ./games/performace-monitor.nix
+    #### emulador
+    ./games/emulador/dolphin.nix
+    ./games/emulador/retroarch.nix
+    ./games/emulador/pcsx2.nix
+    ./games/emulador/rpcs3.nix
+    ./games/emulador/duckstation.nix
+    #### launcher
+    ./games/launcher/bottles.nix
+    ./games/launcher/heroic.nix
+    ./games/launcher/lutris.nix
+    ./games/launcher/prism.nix
+    ## Generic
+    ./generic/discord.nix
+    ./generic/flowtime.nix
+    ./generic/foliate.nix
+    ./generic/gimp.nix
+    ./generic/keepassxc.nix
+    ./generic/obs.nix
+    ./generic/obsidian.nix
+    ./generic/pcloud.nix
+    ./generic/pdfarranger.nix
+    ./generic/qbittorrent.nix
+    ./generic/thunderbird.nix
+    ./generic/zoom.nix
+    ## media
+    ./media/mpd.nix
+    ./media/mpv.nix
+    ./media/stremio.nix
+    ./media/vlc.nix
+    ## Services
+    ./services/syncthing.nix
+    ## Shell
+    ./shell/gpg-agent.nix
     ./shell/zsh.nix
-    ./shell/git.nix
-
-    # ./dev/cc.nix
-    # ./dev/clojure.nix
-    ./dev/docker.nix
-    ./dev/editorconfig.nix
-    # ./dev/go.nix
-    ./dev/html.nix
-    # ./dev/java.nix
-    ./dev/javascript.nix
-    ./dev/lua.nix
-    # ./dev/markdown.nix
-    ./dev/nix.nix
-    # ./dev/php.nix
-    # ./dev/python.nix
-    # ./dev/rust.nix
-    ./dev/shell.nix
-    ./dev/sql.nix
-
-    ./packages.nix
+    ## Term
+    ./term/foot.nix
+    ./term/kitty.nix
+    ./term/wezterm.nix
   ];
 }
