7bb929cb4de72e400c35dd91618686768afa40b1
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-08 12:21:42 -0300

    add setting to disable bluetooth latency

--- hosts/common/hardware/xbox-controller.nix
+++ hosts/common/hardware/xbox-controller.nix
@@ -1,3 +1,12 @@
 {
-  hardware.xpadneo.enable = true;
+  hardware = {
+    xpadneo.enable = true;
+    bluetooth.settings = {
+      General = {
+        MinConnectionInterval = 7;
+        MaxConnectionInterval = 9;
+        ConnectionLatency = 0;
+      };
+    };
+  };
 }
